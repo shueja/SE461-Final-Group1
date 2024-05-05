3230b9d06413b2e1c7e2386ed95cdc980ace43b2
Sawyer <121059512+sawyersieja@users.noreply.github.com>
2024-05-04 21:17:29 -0700

    Most up to date changes to testUpdateGame.

--- test/org/psnbtech/SnakeGameTest.java
+++ test/org/psnbtech/SnakeGameTest.java
@@ -27,19 +27,34 @@ public class SnakeGameTest {
 
 	
 
-	void testUpdateGame(TileType collision, int expectedFruitsEaten, boolean expectedGameOver, boolean expectedPaused) {
+void testUpdateGame(TileType collision, int expectedFruitsEaten, int expectedFruitScore, boolean expectedGameOver, boolean expectedPaused) {
 		
 		game.initGame();
-		game.resetGame();
+		game.isNewGame = false;
+		game.isGameOver = false;
+		game.score = game.getScore();
+		game.fruitsEaten = game.getFruitsEaten();
+		game.nextFruitScore = game.getNextFruitScore();
+		game.spawnFruit();
 		
 		//getting NullPointerException: Cannot read field "x" because "point" is null
 		//testing random initialization value
-		Point initialPosition = game.snake.peekFirst();
+		Point initialPosition = new Point(5, 5);
+		game.snake.clear();
+		game.snake.addFirst(initialPosition);
+				
+		//need to make sure not null
+		game.directions.clear();
+		game.directions.add(Direction.North);
 		
 		//prep board
+		game.board.clearBoard();
 		Point nextPosition = new Point(initialPosition.x, initialPosition.y - 1);
 		game.board.setTile(nextPosition, collision);
 		
+		if(expectedFruitScore < 99) {
+			game.nextFruitScore = 10;
+		}
 		
 		//Runs logic for collision
 		game.updateGame();
@@ -48,21 +63,27 @@ public class SnakeGameTest {
 		assertEquals(expectedGameOver, game.isGameOver(), "Game over state mismatch.");
 		//assertEquals(expectedScore, game.getScore(), "Score mismatch.");
 		assertEquals(expectedFruitsEaten, game.getFruitsEaten(), "Fruits eaten mismatch.");
-		assertEquals(expectedPaused, game.logicTimer.isPaused, "Game logicTimer paused mismatch.");		
+		assertEquals(expectedPaused, game.logicTimer.isPaused, "Game logicTimer paused mismatch.");
+		assertEquals(expectedFruitScore, game.nextFruitScore, "Next fruit score mismatch.");
 	}
 	
 	@Test
 	void testUpdateGameCollisionWithFruit() {
-		testUpdateGame(TileType.Fruit, 1, false, false);//removed int expectedScore	
+		testUpdateGame(TileType.Fruit, 1, 100, false, false);	
 	}
 	
 	@Test
 	void testUpdateGameCollisionWithSnakeBody() {
-		testUpdateGame(TileType.SnakeBody, 0, true, true);//removed int expectedScore	
+		testUpdateGame(TileType.SnakeBody, 0, 100, true, true);	
 	}
 	
 	@Test
-	void testUpdateGameNoCollision() {
-		testUpdateGame(null, 0, false, false);//removed int expectedScore	
+	void testUpdateGameNoCollisionGreaterThan() {
+		testUpdateGame(null, 0, 99, false, false);	
+	}
+	
+	@Test
+	void testUpdateGameNoCollisionNotGreaterThan() {
+		testUpdateGame(null, 0, 10, false, false);	
 	}
 }
